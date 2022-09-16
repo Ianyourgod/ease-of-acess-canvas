4c6b18599504ea305a833e5732842e00033dfa4f
Ianyourgod <ianlouishawthorne@gmail.com>
2022-09-16 17:33:42 -0500

    e

---
+++ index.js
@@ -0,0 +1,9 @@
+class canvas {
+    constructor() {
+        this.canv = document.createElement("canvas")
+        this.canv.width = "100%"
+        this.canv.height = "100%"
+        document.body.appendChild(canv)
+    }
+}
+let canv = new canvas();
