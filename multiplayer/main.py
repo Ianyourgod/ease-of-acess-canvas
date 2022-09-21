649da68098954a2df661699ff25456306c621470
Ianyourgod <ianlouishawthorne@gmail.com>
2022-09-20 21:56:36 -0500

    added socketio

--- multiplayer/main.py
+++ multiplayer/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request,redirect
 from flask_socketio import SocketIO, send, emit
+players = {}
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -7,6 +8,10 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@socketio.on("move")
+def move(name,dir):
+
 
 if __name__ == "__main__":
     app.run(debug=True)
